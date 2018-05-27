fb65124136473985c2f7bf3ed70fba0a3274031f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-import fresh from "@unction/fresh"
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import mergeDeepRight from "@unction/mergedeepright"
-import recordFrom from "@unction/recordfrom"
-import split from "@unction/split"
-
-export default function inflateTree (delimiter: StringType): UnaryFunctionType {
-  return function inflateTreeDelimiter (record: RecordType): NestedRecordType {
-    return reduceWithValueKey(
-      (accumulated: RecordType | NestedRecordType): UnaryFunctionType =>
-        (value: mixed): UnaryFunctionType =>
-          (key: KeyType): NestedRecordType =>
-            mergeDeepRight(recordFrom(split(delimiter)(key))(value))(accumulated)
-    )(
-      fresh(record)
-    )(
-      record
-    )
-  }
+import fresh from "@unction/fresh";
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import mergeDeepRight from "@unction/mergedeepright";
+import recordFrom from "@unction/recordfrom";
+import split from "@unction/split";
+export default function inflateTree(delimiter) {
+  return function inflateTreeDelimiter(record) {
+    return reduceWithValueKey(accumulated => value => key => mergeDeepRight(recordFrom(split(delimiter)(key))(value))(accumulated))(fresh(record))(record);
+  };
 }
