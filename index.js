193e0066e530712a52de8f94f62d064e4d593841
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-06-03 23:12:30 -0700

    Switching away from deprecated functions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import fresh from "@unction/fresh";
 import reduceWithValueKey from "@unction/reducewithvaluekey";
 import mergeDeepRight from "@unction/mergedeepright";
-import recordFrom from "@unction/recordfrom";
+import objectFrom from "@unction/objectfrom";
 import split from "@unction/split";
+
 export default function inflateTree (delimiter) {
   return function inflateTreeDelimiter (record) {
-    return reduceWithValueKey((accumulated) => (value) => (key) => mergeDeepRight(recordFrom(split(delimiter)(key))(value))(accumulated))(fresh(record))(record);
+    return reduceWithValueKey((accumulated) => (value) => (key) => mergeDeepRight(objectFrom(split(delimiter)(key))(value))(accumulated))(fresh(record))(record);
   };
 }
